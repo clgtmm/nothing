e9d5cb415f00b573abf6da5a765f29fbc7196fe3
clgtmm <103909534+clgtmm@users.noreply.github.com>
2023-03-03 11:12:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("baanhdeptrai")
-print("you're wanderer just like me")
+print(
+    "1. Deity Hub\n"
+    "2. W-azure Hub\n"
+    "3.Sea hub"
+)
+deityhubscript = input("your choice:")
+if deityhubscript == "1":
+    script = requests.get("https://pastebin.com/raw/D5vHuHXz").text
+    print(script)
+if deityhubscript == "2":
+    script = requests.get("https://pastebin.com/raw/uyptWCd1").text
+    print(script)
+if deityhubscript == "3":
+    script = requests.get("https://pastebin.com/raw/v62nzyfD3").text
+    print(script)
